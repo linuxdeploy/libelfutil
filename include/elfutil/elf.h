a9eb9625ced2187dc668bc6bb71f3f2947844b8e
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-10-16 22:03:41 +0200

    Copy of the elf related files from linuxdeploy

---
+++ include/elfutil/elf.h
@@ -0,0 +1,70 @@
+// system includes
+#include <vector>
+#include <string>
+// including system elf header, which allows for interpretation of the return values of the methods
+#include <elf.h>
+
+// library includes
+#include <boost/filesystem.hpp>
+
+#pragma once
+
+namespace elfutil {
+    // thrown by constructor if file is not an ELF file
+    class ElfFileParseError : public std::runtime_error {
+    public:
+        explicit ElfFileParseError(const std::string& msg) : std::runtime_error(msg) {}
+    };
+
+    // thrown by traceDynamicDependencies() if a dependency is missing
+    class DependencyNotFoundError : public std::runtime_error {
+    public:
+        explicit DependencyNotFoundError(const std::string& msg) : std::runtime_error(msg) {}
+    };
+
+    class ElfFile {
+    private:
+        class PrivateData;
+
+        PrivateData* d;
+
+    public:
+        explicit ElfFile(const boost::filesystem::path& path);
+
+        ~ElfFile();
+
+    public:
+        // return system ELF OS ABI
+        static uint8_t getSystemElfABI();
+
+        // return system ELF class (32-bit or 64-bit)
+        static uint8_t getSystemElfClass();
+
+        // return system (ELF) endianness
+        static uint8_t getSystemElfEndianness();
+
+    public:
+        // recursively trace dynamic library dependencies of a given ELF file
+        // this works for both libraries and executables
+        // the resulting vector consists of absolute paths to the libraries determined by the same methods a system's
+        // linker would use
+        std::vector<boost::filesystem::path> traceDynamicDependencies();
+
+        // fetch rpath stored in binary
+        // it appears that according to the ELF standard, the rpath is ignored in libraries, therefore if the path
+        // points to an executable, an empty string is returned
+        std::string getRPath();
+
+        // set rpath in ELF file
+        // returns true on success, false otherwise
+        bool setRPath(const std::string& value);
+
+        // return ELF class
+        uint8_t getElfClass();
+
+        // return OS ABI
+        uint8_t getElfABI();
+    };
+}
+
+
---
+++ src/elfutil/misc.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <algorithm>
+#include <climits>
+#include <cstring>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace elfutil {
+    namespace misc {
+        static inline bool ltrim(std::string& s, char to_trim = ' ') {
+            // TODO: find more efficient way to check whether elements have been removed
+            size_t initialLength = s.length();
+            s.erase(s.begin(), std::find_if(s.begin(), s.end(), [to_trim](int ch) {
+                return ch != to_trim;
+            }));
+            return s.length() < initialLength;
+        }
+
+        static inline bool rtrim(std::string& s, char to_trim = ' ') {
+            // TODO: find more efficient way to check whether elements have been removed
+            auto initialLength = s.length();
+            s.erase(std::find_if(s.rbegin(), s.rend(), [to_trim](int ch) {
+                return ch != to_trim;
+            }).base(), s.end());
+            return s.length() < initialLength;
+        }
+
+        static inline bool trim(std::string& s, char to_trim = ' ') {
+            // returns true if either modifies s
+            auto ltrim_result = ltrim(s, to_trim);
+            return rtrim(s, to_trim) && ltrim_result;
+        }
+
+        static std::vector <std::string> split(const std::string& s, char delim = ' ') {
+            std::vector <std::string> result;
+
+            std::stringstream ss(s);
+            std::string item;
+
+            while (std::getline(ss, item, delim)) {
+                result.push_back(item);
+            }
+
+            return result;
+        }
+
+        static std::vector <std::string> splitLines(const std::string& s) {
+            return split(s, '\n');
+        }
+
+        static inline std::string strLower(std::string s) {
+            std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::tolower(c); });
+            return s;
+        }
+
+        static bool stringStartsWith(const std::string& string, const std::string& prefix) {
+            // sanity check
+            if (string.size() < prefix.size())
+                return false;
+
+            return strncmp(string.c_str(), prefix.c_str(), prefix.size()) == 0;
+        }
+
+        static bool stringEndsWith(const std::string& string, const std::string& suffix) {
+            // sanity check
+            if (string.size() < suffix.size())
+                return false;
+
+            return strcmp(string.c_str() + (string.size() - suffix.size()), suffix.c_str()) == 0;
+        }
+
+        static bool stringContains(const std::string& string, const std::string& part) {
+            return string.find(part) != std::string::npos;
+        }
+
+        static std::string getOwnExecutablePath() {
+            // FIXME: reading /proc/self/exe line is Linux specific
+            std::vector<char> buf(PATH_MAX, '\0');
+
+            if (readlink("/proc/self/exe", buf.data(), buf.size()) < 0) {
+                return "";
+            }
+
+            return buf.data();
+        }
+    }
+}
